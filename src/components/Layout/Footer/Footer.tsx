e54da3695cbda008eef4f6aa2597d09bd5080353
Konstantinos Lepidas <kolepidas@gmail.com>
2024-09-22 16:56:12 +0300

    Initial commit

--- src/components/Layout/Footer/Footer.tsx
+++ src/components/Layout/Footer/Footer.tsx
@@ -5,19 +5,14 @@ export const Footer = () => {
   return (
     <footer className='mx-auto w-full max-w-prose pb-6 pl-6 pr-6 text-center text-gray-400'>
       <div className='flex flex-col items-center text sm text-gray-400'>
-        <a
-          className='text-gray-400 text-sm hover:cursor-pointer hover:underline'
-          href='/disclaimer'
-        >
           Disclaimer
-        </a>
         <a
           target='_blank'
           className='flex items-center text-sm hover:underline'
           href='https://multiversx.com/'
         >
-          Made with <Image src={heartIcon} alt='love' className='mx-1' /> by the
-          MultiversX team
+          Made with <Image src={heartIcon} alt='love' className='mx-1' /> by
+          QuantumX Network team
         </a>
       </div>
     </footer>
